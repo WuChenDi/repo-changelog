c440df5777503b773a7e1728e4f02106be16ead5
wudi <wuchendi96@gmail.com>
2025-09-11 19:20:35 +0800

    build(nuxt): disable sourcemap and enable SSR
    - Disable both server and client sourcemaps
    - Enable SSR (Server-Side Rendering)
    - Add @nuxt/eslint and @nuxt/ui modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  sourcemap: {
+    server: false,
+    client: false,
+  },
+  ssr: false,
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
